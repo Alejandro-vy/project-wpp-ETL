ba08d89aea3834043502ad41006ecffba828c692
Alejandro_v <a.vera07@ufromail.cl>
2025-07-03 20:48:01 -0400

    Agregamos un Dag

--- airflow/dags/wpp_etl_dag.py
+++ airflow/dags/wpp_etl_dag.py
@@ -1,20 +1,38 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime
+from airflow.utils.dates import days_ago
 
-# Define tu DAG
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+}
+
 with DAG(
-    dag_id='wpp_etl_dag',
-    start_date=datetime(2024, 1, 1),
-    schedule_interval=None,   # Manual
-    catchup=False
+    dag_id='wpp_etl_pipeline',
+    default_args=default_args,
+    description='ETL pipeline for World Population Prospects data',
+    schedule_interval=None,  # Ponlo None para lanzarlo manual al inicio
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['ETL', 'wpp', 'population'],
 ) as dag:
 
-    # Task: Ejecutar tu script de extracción
-    extract_population = BashOperator(
-        task_id='extract_population',
+    extract = BashOperator(
+        task_id='extract_data',
         bash_command='python /opt/airflow/scripts/extract_wpp.py'
     )
 
-    extract_population
+    transform = BashOperator(
+        task_id='transform_data',
+        bash_command='python /opt/airflow/scripts/transform_wpp.py'
+    )
+
+    load = BashOperator(
+        task_id='load_data',
+        bash_command='python /opt/airflow/scripts/load_wpp.py'
+    )
+
+    extract >> transform >> load
+
+
 
